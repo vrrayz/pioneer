e33000ea5be26c2528fd8488080adfa03215e5f7
Victor <emmyvic98@gmail.com>
2024-09-03 20:08:48 +0100

    react snap setup

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react'
-import { hydrate, render } from 'react-dom'
+import ReactDOM from 'react-dom'
 
 import { App } from './app'
-
-const rootElement = document.getElementById('root')
-if (rootElement?.hasChildNodes()) {
-  hydrate(<App />, rootElement)
-} else {
-  render(<App />, rootElement)
-}
-// ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
